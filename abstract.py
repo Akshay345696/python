d8609e16430cb48e4037404afbd893c03e302489
Akshay345696 <84923289+Akshay345696@users.noreply.github.com>
2023-10-31 06:44:21 -0700

    Add files via upload

--- abstract.py
+++ abstract.py
@@ -1,38 +1,5 @@
-from abc import ABC, abstractmethod
-class Polygon(ABC):
-    @abstractmethod
-    def noofsides(self):
-        pass
-
-class Triangle(Polygon):
-
-    def noofsides(self):
-        print("I have 3 sides")
-class Quadrilateral(Polygon):
-
-    def noofsides(self):
-        print("I have 4 sides")
-
-class Pentagon(Polygon):
-
-    def noofsides(self):
-        print("I have 5 sides")
+thisdict={"name":"akshay" ,"year":2002}
+print(thisdict["name"])
 
 
-class Hexagon(Polygon):
 
-    def noofsides(self):
-        print("I have 6 sides")
-
-
-R = Triangle()
-R.noofsides()
-
-k = Quadrilateral()
-k.noofsides()
-
-R = Pentagon()
-R.noofsides()
-
-k = Hexagon()
-k.noofsides()
